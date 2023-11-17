e3a17241ab2401296447e69153d8adc0094d8b1e
shivangbajaj <baja0890@mylaurier.ca>
2023-11-17 01:49:09 -0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -10,6 +10,12 @@ app.get('/', (req, res) => {
     });
 });
 
+app.get('/hw', (req, res) => {
+    return res.status(200).json({
+        message: 'Hello World!!!!'
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
