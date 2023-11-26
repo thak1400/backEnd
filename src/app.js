32391ec57cc402ee8a43c7291f45c5ffe09cff80
rishikeshravi3 <137996382+rishikeshravi3@users.noreply.github.com>
2023-11-26 17:34:30 -0500

    recommened jobs api added

--- src/app.js
+++ src/app.js
@@ -493,3 +493,29 @@ export const getApplicationsByEmail = async (request, response) => {
     }
     
 }
+
+export const getRecommendedJobs = async (request, response) => {
+    try{
+        if(request.body && typeof request.body === ('object')) {
+            const shwiftRepo = new ShwiftRepository();
+            const {emailId} = request.body;
+            console.log(emailId);
+            const result = await shwiftRepo.getRecommendedJobs(emailId);
+            if(result) {
+                console.log(`getRecommendedJobs successful`);
+                response.status(200).send(result);
+            } else {
+                console.error(`getRecommendedJobs failed  - ${JSON.stringify(request.body)}`);
+                response.status(400).send({
+                    type: 'BAD_REQUEST',
+                    message: 'Request failed before completion',
+                    details: 'Invalid Input request'
+                });
+            }
+        }
+    } catch(error) {
+        console.log(`getRecommendedJobs failed - ${JSON.stringify(error)}`);
+        response.status(500).send(error);
+    }
+    
+}
