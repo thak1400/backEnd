51c4ad713723af35fcfef35b1541098ff996ade0
shivangbajaj <baja0890@mylaurier.ca>
2023-11-26 19:38:39 -0500

    Adding availability to signup

--- src/repository.js
+++ src/repository.js
@@ -315,8 +315,8 @@ async signUp(serialNum,userData) {
             
             if(dbResultNewAccount.rows[0].acc_type.toLowerCase()==="employee")
             {
-                const signUpInfo=`INSERT into shwift.employeeinfo (first_name,last_name,employee_id,curr_employment_status,employee_gender,employee_dob,emp_expertise,rating,employee_dp,curr_position,emp_summary,emp_projects,emp_skills,emp_workex,emp_education,serial_num)
-                values ('${dbResultNewAccount.rows[0].first_name}','${dbResultNewAccount.rows[0].last_name}','${dbResultNewAccount.rows[0].email_id}','','','','',0,'${userData.profileDp}','','','','','','','${serialNum}') RETURNING *;`;
+                const signUpInfo=`INSERT into shwift.employeeinfo (first_name,last_name,employee_id,curr_employment_status,employee_gender,employee_dob,emp_expertise,rating,employee_dp,curr_position,emp_summary,emp_projects,emp_skills,emp_workex,emp_education,serial_num, availability)
+                values ('${dbResultNewAccount.rows[0].first_name}','${dbResultNewAccount.rows[0].last_name}','${dbResultNewAccount.rows[0].email_id}','','','','',0,'${userData.profileDp}','','','','','','','${serialNum}','') RETURNING *;`;
                 console.log(signUpInfo);
                 const dbsignUpInfo=await connection.dbClient.query(signUpInfo);
             }
